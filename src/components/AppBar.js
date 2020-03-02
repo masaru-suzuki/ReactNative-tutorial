2592c58c30c5c00cb3d1df9769647547f6abbeda
masaru-suzuki <masaru.suzuki.yamagata@gmail.com>
2020-03-02 15:56:13 +0900

    create AppBar as modules

---
+++ src/components/AppBar.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+class Appbar extends React.Component {
+  render() {
+    return(
+      <View style={styles.appbar}>
+        <View>
+          <Text style={styles.appbarTitle}>MEMORIZE</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  appbar: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    left: 0,
+    paddingTop: 30,
+    backgroundColor: '#b3afaf',
+    height: 78,
+    lineHeight: 78,
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.4,
+    shadowRadius: 3,
+    zIndex: 2
+  },
+  appbarTitle: {
+    fontSize: 22,
+    color: '#444'
+  },
+});
+
+export default Appbar;
