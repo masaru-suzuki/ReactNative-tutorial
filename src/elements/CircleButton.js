287e09bdc7ee9736a700e8411f7a7317a8f982fc
masaru-suzuki <masaru.suzuki.yamagata@gmail.com>
2020-03-02 15:57:11 +0900

    create CircleButton as modules

---
+++ src/elements/CircleButton.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+class CircleButton extends React.Component {
+  render() {
+    return(
+      <View style={styles.memoAddBtn}>
+        <Text style={styles.addBtn}>＋</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  memoAddBtn: {
+    height: 50,
+    width: 50,
+    backgroundColor: '#b3afaf',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 25,
+    position: 'absolute',
+    bottom: 30,
+    right: 30,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.4,
+    shadowRadius: 3
+  },
+  addBtn: {
+    fontSize: 25,
+    lineHeight: 25,
+    opacity: 0.5
+  }
+});
+
+export default CircleButton;
