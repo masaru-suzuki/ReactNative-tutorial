71f38a75dbbeba204b4366921c6a113496437eec
masaru-suzuki <masaru.suzuki.yamagata@gmail.com>
2020-03-03 09:57:58 +0900

    fix CercleButton

--- src/elements/CircleButton.js
+++ src/elements/CircleButton.js
@@ -3,8 +3,24 @@ import {StyleSheet, Text, View} from 'react-native';
 
 class CircleButton extends React.Component {
   render() {
+    // const style = this.props.style;
+
+  // ボタンの色の使い分け方法
+    // const {style, color} = this.props;
+    // let bgColor = '#b3afaf';
+    // let txtColor = '#111';
+    
+    // if(color === 'white') {
+      //   bgColor = '#B3AFAF';
+      //   txtColor = '#111';
+      // }
+      const {style} = this.props;
+
     return(
-      <View style={styles.circleBtn}>
+      //ここのstyleの付け方慣れていない
+      // <View style={[styles.circleBtn, style, {backgroundColor: bgColor}]}>
+      //   <Text style={[styles.addBtn, {color: txtColor}]}>{this.props.children}</Text>
+      <View style={[styles.circleBtn, style]}>
         <Text style={styles.addBtn}>{this.props.children}</Text>
       </View>
     );
@@ -15,13 +31,13 @@ const styles = StyleSheet.create({
   circleBtn: {
     height: 50,
     width: 50,
-    backgroundColor: '#b3afaf',
     alignItems: 'center',
     justifyContent: 'center',
     borderRadius: 25,
     position: 'absolute',
     bottom: 30,
     right: 30,
+    backgroundColor: '#b3afaf',
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 1 },
     shadowOpacity: 0.4,
